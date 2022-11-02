6af9dcc9c9edd304b2749d582ee1b96c0dc9ca4f
Vitor Hugo <84356731+vitorugoc@users.noreply.github.com>
2022-11-02 13:26:27 -0300

    customizate feedback logs

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -5,17 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\SiteContato;
 use Illuminate\Http\Request;
+use App\Models\MotivoContato;
 
 class ContatoController extends Controller
 {
 
-    public function contato(Request $request)
+    public function contato()
     {
-        $motivo_contatos = [
-            1 => 'Dúvida',
-            2 => 'Elogio',
-            3 => 'Reclamação'
-        ];
+        $motivo_contatos = MotivoContato::all();
 
         return view('site.contato', ['motivo_contatos' => $motivo_contatos]);
     }
@@ -23,17 +20,29 @@ class ContatoController extends Controller
     public function salvar(Request $request){
         $contato = new SiteContato();
 
-        $request->validate([
+        $regras = [
             'nome' => 'required|min:3|max:4014',
             'telefone' => 'required',
-            'email' => 'required',
-            'motivo_contato' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
+        ];
+        
+        $feedback = [
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres.',
+            'nome.max' => 'O campo nome precisa ter no máximo 40 caracteres.',
+            'email.email' => 'O email informado não é válido',
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
+            'required' => 'O campo :attribute deve ser preenchido.',
+
+        ];
+
+        $request->validate($regras, $feedback);
 
         if(!empty($request->all())){
             $contato->fill($request->all());
             $contato->save();
         }
+        return redirect()->route('site.index');
     }
 }
--- app_super_gestao/app/Http/Controllers/PrincipalController.php
+++ app_super_gestao/app/Http/Controllers/PrincipalController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\MotivoContato;
 
 class PrincipalController extends Controller
 {
 
     public function principal()
     {
-        return view('site.principal');
+        $motivo_contatos = MotivoContato::all();
+
+        return view('site.principal', ['motivo_contatos' => $motivo_contatos]);
     }
 }
