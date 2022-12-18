9e6036dcf49a4a50698860c38fa2408deec70b1d
Vitor Hugo <vitorhugo02cc@gmail.com>
2022-12-18 16:29:24 -0300

    teste

--- app_controle_tarefas/resources/views/auth/register.blade.php
+++ app_controle_tarefas/resources/views/auth/register.blade.php
@@ -64,7 +64,7 @@
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Cadastrar novo usuário') }}
+                                    {{ __('Cadastrar novo usuárie') }}
                                 </button>
                             </div>
                         </div>
