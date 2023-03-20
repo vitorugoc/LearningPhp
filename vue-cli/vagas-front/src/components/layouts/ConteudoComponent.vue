1d591e17c34dd59c2160c6df920930144354841f
vitorugoc <vitorhugo02cc@gmail.com>
2023-03-20 11:06:46 -0300

    New: lifecycle hooks

--- vue-cli/vagas-front/src/components/layouts/ConteudoComponent.vue
+++ vue-cli/vagas-front/src/components/layouts/ConteudoComponent.vue
@@ -22,6 +22,45 @@ export default {
     HomeView: HomeView,
     PublicarVagaView: PublicarVagaView
 
+  },
+  beforeCreate(){
+    console.log('Antes de criar')
+  },
+  created(){
+    console.log('Criado')
+  },
+  beforeMount(){
+    console.log("Antes de montar")
+  },
+  mounted(){
+    console.log('Montado')
+  },
+  beforeUpdate(){
+    console.log('Antes de atualizar')
+  },
+  updated(){
+    console.log('atualizado')
+  },
+  beforeUnmount(){
+    console.log('Antes de desmontar')
+  },
+  unmounted(){
+    console.log('Desmontado')
+  },
+  errorCaptured(){
+    console.log('Erro capturado')
+  },
+  renderTracked(){
+    console.log('Re-renderização rastreada')
+  },
+  renderTriggered(){
+    console.log('Re-renderizaçao acionada')
+  },
+  activated(){
+    console.log('Componente é ativado')
+  },
+  deactivated(){
+    console.log('Componente desativado')
   }
 }
 </script>
