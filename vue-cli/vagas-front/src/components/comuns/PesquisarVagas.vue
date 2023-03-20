ec32a145168e094125de270a28d7f488848296fa
vitorugoc <vitorhugo02cc@gmail.com>
2023-03-20 14:55:49 -0300

    New: formatação

--- vue-cli/vagas-front/src/components/comuns/PesquisarVagas.vue
+++ vue-cli/vagas-front/src/components/comuns/PesquisarVagas.vue
@@ -1,27 +1,32 @@
 <template>
-    <div class="h-100 p-5 bg-light border rounded-3">
-        <h2>Pesquisar Vagas</h2>
-        <div class="row">
-            <div class="col">
-                <div class="form-group">
-                    <label>Título da Vaga</label>
-                    <input type="text" class="form-control"
-                        placeholder="Pesquise por palavras chaves, por exemplo PHP, Pleno, Analista">
-                    <small class="form-text text-muted">Informe palavras que estejam relacionadas com o título da vaga que
-                        você procura</small>
-                </div>
-            </div>
+  <div class="h-100 p-5 bg-light border rounded-3">
+    <h2>Pesquisar Vagas</h2>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label>Título da Vaga</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Pesquise por palavras chaves, por exemplo PHP, Pleno, Analista"
+          />
+          <small class="form-text text-muted"
+            >Informe palavras que estejam relacionadas com o título da vaga que você
+            procura</small
+          >
         </div>
-        <div class="row">
-            <div class="col">
-                <button class="btn btn-outline-dark mt-2">Buscar</button>
-            </div>
-        </div>
+      </div>
     </div>
+    <div class="row">
+      <div class="col">
+        <button class="btn btn-outline-dark mt-2">Buscar</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'PesquisarVagas'
-}
+  name: "PesquisarVagas",
+};
 </script>
