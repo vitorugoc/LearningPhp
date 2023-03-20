17e16eab7a8e3ec2f1e4eb01687ee3440284086d
vitorugoc <vitorhugo02cc@gmail.com>
2023-03-20 14:51:51 -0300

    New: template home e pesquisarvagas

--- vue-cli/vagas-front/src/components/views/HomeView.vue
+++ vue-cli/vagas-front/src/components/views/HomeView.vue
@@ -1,26 +1,53 @@
 <template>
-  <div>
-    <h1>Componente Home</h1>
-  </div>
+    <div class="container py-4">
+      <div class="row">
+        <div class="col">
+          <PesquisarVagas></PesquisarVagas>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-4">
+          <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+            <p>Vagas Abertas</p>
+            <h2>25</h2>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+            <p>Profissionais Cadastrados</p>
+            <h2>125</h2>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="h-100 p-5 rounded-3 border bg-light text-dark">
+            <p>Visitantes Online</p>
+            <h2>15</h2>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
+import PesquisarVagas from '../comuns/PesquisarVagas.vue';
 export default {
   name: "HomeView",
-  activated(){
-    console.log('Componente é ativado')
+  components: {
+    PesquisarVagas
   },
-  deactivated(){
-    console.log('Componente é desativadp')
+  activated() {
+    console.log("Componente é ativado");
   },
-  beforeUnmount(){
-    console.log('Antes de desmontar')
+  deactivated() {
+    console.log("Componente é desativadp");
   },
-  unmounted(){
-    console.log('Desmontado')
-  }
+  beforeUnmount() {
+    console.log("Antes de desmontar");
+  },
+  unmounted() {
+    console.log("Desmontado");
+  },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
