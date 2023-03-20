5a7a9cac219ce4f55cb16e56c844629c4e8a47f7
vitorugoc <vitorhugo02cc@gmail.com>
2023-03-20 14:55:33 -0300

    New: formatação

--- vue-cli/vagas-front/src/components/views/HomeView.vue
+++ vue-cli/vagas-front/src/components/views/HomeView.vue
@@ -1,39 +1,39 @@
 <template>
-    <div class="container py-4">
-      <div class="row">
-        <div class="col">
-          <PesquisarVagas></PesquisarVagas>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col">
+        <PesquisarVagas></PesquisarVagas>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-4">
+        <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+          <p>Vagas Abertas</p>
+          <h2>25</h2>
         </div>
       </div>
-      <div class="row mt-5">
-        <div class="col-4">
-          <div class="h-100 p-5 rounded-3 border bg-dark text-white">
-            <p>Vagas Abertas</p>
-            <h2>25</h2>
-          </div>
+      <div class="col-4">
+        <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+          <p>Profissionais Cadastrados</p>
+          <h2>125</h2>
         </div>
-        <div class="col-4">
-          <div class="h-100 p-5 rounded-3 border bg-dark text-white">
-            <p>Profissionais Cadastrados</p>
-            <h2>125</h2>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="h-100 p-5 rounded-3 border bg-light text-dark">
-            <p>Visitantes Online</p>
-            <h2>15</h2>
-          </div>
+      </div>
+      <div class="col-4">
+        <div class="h-100 p-5 rounded-3 border bg-light text-dark">
+          <p>Visitantes Online</p>
+          <h2>15</h2>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
-import PesquisarVagas from '../comuns/PesquisarVagas.vue';
+import PesquisarVagas from "../comuns/PesquisarVagas.vue";
 export default {
   name: "HomeView",
   components: {
-    PesquisarVagas
+    PesquisarVagas,
   },
   activated() {
     console.log("Componente é ativado");
