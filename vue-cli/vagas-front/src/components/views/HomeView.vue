788350cb6b6960b44c71938d58480bf87eb8ef22
vitorugoc <vitorhugo02cc@gmail.com>
2023-03-21 11:10:21 -0300

    New: Gerando vagas dinamicamente através de props

--- vue-cli/vagas-front/src/components/views/HomeView.vue
+++ vue-cli/vagas-front/src/components/views/HomeView.vue
@@ -5,6 +5,19 @@
         <PesquisarVagas></PesquisarVagas>
       </div>
     </div>
+
+    <div class="row mt-5" v-for="(vaga, index) in vagas" :key="index">
+      <div class="col">
+        <VagaComponent
+          :titulo="vaga.titulo"
+          :descricao="vaga.descricao"
+          :salario="vaga.salario"
+          :modalidade="vaga.modalidade"
+          :tipo="vaga.tipo"
+          :publicacao="vaga.publicacao"
+        />
+      </div>
+    </div>
     <div class="row mt-5">
       <div class="col-4">
         <IndicadorComponent
@@ -37,14 +50,61 @@
 <script>
 import PesquisarVagas from "../comuns/PesquisarVagas.vue";
 import IndicadorComponent from "../comuns/IndicadorComponent.vue";
+import VagaComponent from "../comuns/VagaComponent.vue";
+
 export default {
   name: "HomeView",
   components: {
     PesquisarVagas,
     IndicadorComponent,
+    VagaComponent,
   },
   data: () => ({
     usuariosOnline: 0,
+    vagas: [
+      {
+        titulo: "Analista Programador PHP Pleno",
+        descricao:
+          "Profissional com conhecimentos em PHP, Laravel e MySQL. Necessário 3 anos de experiências. Atuará na manutenção de sistemas legados da empresa.",
+        salario: 6000,
+        modalidade: "Home Office",
+        tipo: "PJ",
+        publicacao: "2021-10-10",
+      },
+      {
+        titulo: "Programador JavaScript Angular",
+        descricao: "Profissional com conhecimentos avançados em JavaScript e Angular.",
+        salario: 5000,
+        modalidade: "Presencial",
+        tipo: "CLT",
+        publicacao: "2021-10-07",
+      },
+      {
+        titulo: "Programador JavaScript Vue",
+        descricao: "Profissional com conhecimentos avançados em JavaScript e Vue.",
+        salario: 5000,
+        modalidade: "Home Office",
+        tipo: "CLT",
+        publicacao: "2021-10-06",
+      },
+      {
+        titulo: "Analista de Banco de Dados Sênior",
+        descricao: "Domínio dos bancos de dados SQL Server, Oracle, Postgre e MySQL",
+        salario: 9000,
+        modalidade: "Presencial",
+        tipo: "PJ",
+        publicacao: "2021-10-06",
+      },
+      {
+        titulo: "Programador Web Júnior",
+        descricao:
+          "Conhecimentos básicos em HTML, CSS, JavaScript, Bootstrap, PHP e MySQL",
+        salario: 3000,
+        modalidade: "Presencial",
+        tipo: "CLT",
+        publicacao: "2021-10-05",
+      },
+    ],
   }),
   methods: {
     getUsuariosOnline() {
