17bbee88ceba6520efea4cefad88a67fa8fc3650
vitorugoc <vitorhugo02cc@gmail.com>
2023-03-27 14:30:04 -0300

    New: Exibindo as vagas do localStorage

--- vue-cli/vagas-front/src/components/views/HomeView.vue
+++ vue-cli/vagas-front/src/components/views/HomeView.vue
@@ -54,50 +54,7 @@ export default {
   },
   data: () => ({
     usuariosOnline: 0,
-    vagas: [
-      {
-        titulo: "Analista Programador PHP Pleno",
-        descricao:
-          "Profissional com conhecimentos em PHP, Laravel e MySQL. Necessário 3 anos de experiências. Atuará na manutenção de sistemas legados da empresa.",
-        salario: 6000,
-        modalidade: "Home Office",
-        tipo: "PJ",
-        publicacao: "2021-10-10",
-      },
-      {
-        titulo: "Programador JavaScript Angular",
-        descricao: "Profissional com conhecimentos avançados em JavaScript e Angular.",
-        salario: 5000,
-        modalidade: "Presencial",
-        tipo: "CLT",
-        publicacao: "2021-10-07",
-      },
-      {
-        titulo: "Programador JavaScript Vue",
-        descricao: "Profissional com conhecimentos avançados em JavaScript e Vue.",
-        salario: 5000,
-        modalidade: "Home Office",
-        tipo: "CLT",
-        publicacao: "2021-10-06",
-      },
-      {
-        titulo: "Analista de Banco de Dados Sênior",
-        descricao: "Domínio dos bancos de dados SQL Server, Oracle, Postgre e MySQL",
-        salario: 9000,
-        modalidade: "Presencial",
-        tipo: "PJ",
-        publicacao: "2021-10-06",
-      },
-      {
-        titulo: "Programador Web Júnior",
-        descricao:
-          "Conhecimentos básicos em HTML, CSS, JavaScript, Bootstrap, PHP e MySQL",
-        salario: 3000,
-        modalidade: "Presencial",
-        tipo: "CLT",
-        publicacao: "2021-10-05",
-      },
-    ],
+    vagas: [],
   }),
   methods: {
     getUsuariosOnline() {
@@ -108,16 +65,7 @@ export default {
     setInterval(this.getUsuariosOnline, 1000);
   },
   activated() {
-    console.log("Componente é ativado");
-  },
-  deactivated() {
-    console.log("Componente é desativado  ");
-  },
-  beforeUnmount() {
-    console.log("Antes de desmontar");
-  },
-  unmounted() {
-    console.log("Desmontado");
+    this.vagas = JSON.parse(localStorage.getItem("vagas"));
   },
 };
 </script>
