0d70234f288addd479c29f412ce130e234977ee4
vitorugoc <vitorhugo02cc@gmail.com>
2023-04-12 15:46:59 -0300

    New: finalizando template vagas favoritas

--- vue-cli/vagas-front/src/App.vue
+++ vue-cli/vagas-front/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <VagasFavoritas></VagasFavoritas>
     <topo-padrao @navegar="componente = $event" />
     <conteudo v-if="visibilidade" :conteudo="componente"></conteudo>
   </div>
@@ -8,6 +9,7 @@
 <script>
 import ConteudoComponent from "./components/layouts/ConteudoComponent.vue";
 import TopoPadrao from "./components/layouts/TopoPadrao.vue";
+import VagasFavoritas from "./components/comuns/VagasFavoritas.vue";
 
 export default {
   name: "App",
@@ -18,6 +20,7 @@ export default {
   components: {
     Conteudo: ConteudoComponent,
     TopoPadrao: TopoPadrao,
+    VagasFavoritas: VagasFavoritas,
   },
 };
 </script>
---
+++ vue-cli/vagas-front/src/components/comuns/VagasFavoritas.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="divVagasFavoritas">
+      <button
+        class="btn btn-primary"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasRight"
+        aria-controls="offcanvasRight"
+      >
+        Vagas Favoritas
+      </button>
+    </div>
+
+    <div
+      class="offcanvas offcanvas-end"
+      tabindex="-1"
+      id="offcanvasRight"
+      aria-labelledby="offcanvasRightLabel"
+    >
+      <div class="offcanvas-header">
+        <h5 id="offcanvasRightLabel">Offcanvas right</h5>
+        <button
+          type="button"
+          class="btn-close text-reset"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="offcanvas-body">...</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VagasFavoritas",
+};
+</script>
+
+<style scoped>
+.divVagasFavoritas {
+  position: absolute;
+  z-index: 1;
+  top: 10px;
+  right: 10px;
+}
+</style>
