05a4e9c9dd3f8b771ea08b991545333944891513
vitorugoc <vitorhugo02cc@gmail.com>
2023-04-18 14:53:38 -0300

    New: limpando registro

--- vue-cli/vagas-front/src/components/views/PublicarVagaView.vue
+++ vue-cli/vagas-front/src/components/views/PublicarVagaView.vue
@@ -89,6 +89,15 @@ export default {
       });
 
       localStorage.setItem("vagas", JSON.stringify(vagas));
+
+      this.resetaFormularioCadastroVaga();
+    },
+    resetaFormularioCadastroVaga() {
+      this.titulo = "";
+      this.descricao = "";
+      this.salario = "";
+      this.modalidade = "";
+      this.tipo = "";
     },
   },
 };
