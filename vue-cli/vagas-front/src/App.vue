5b2a5fea3c974090e99a390a4726fdefef68ed23
vitorugoc <vitorhugo02cc@gmail.com>
2023-04-19 14:58:13 -0300

    New: Utilizando slots

--- vue-cli/vagas-front/src/App.vue
+++ vue-cli/vagas-front/src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <AlertaComponent />
     <VagasFavoritas></VagasFavoritas>
     <topo-padrao @navegar="componente = $event" />
+    <AlertaComponent v-if="exibirAlerta">
+      <div class="alert alert-success" role="alert">
+        <h5>Titulo do alerta</h5>
+        <hr />
+        <p>Descrição do alerta</p>
+      </div>
+    </AlertaComponent>
     <conteudo v-if="visibilidade" :conteudo="componente"></conteudo>
   </div>
 </template>
@@ -28,6 +34,10 @@ export default {
   },
   mounted() {
     this.emitter.on("alerta", () => {
+      this.exibirAlerta = true;
+      setTimeout(() => {
+        this.exibirAlerta = false;
+      }, 4000);
       console.log("Alerta");
     });
   },
--- vue-cli/vagas-front/src/components/comuns/AlertaComponent.vue
+++ vue-cli/vagas-front/src/components/comuns/AlertaComponent.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="alert alert-success" role="alert">
-    Aqui ficará a mensagem de alerta (feedback)
+  <div>
+    <slot></slot>
   </div>
 </template>
 
